2f0eb917def837eb1614f3811d05031055492781
Observer3510 <missjiayiwu@gmail.com>
2024-11-10 12:50:58 -0500

    Implemented input files for enter expense use case.

---
+++ src/main/java/use_case/enter_expense/EnterExpenseInputBoundary.java
@@ -0,0 +1,14 @@
+package use_case.enter_expense;
+
+/**
+ * The Enter Expense Use Case.
+ */
+public interface EnterExpenseInputBoundary {
+
+    /**
+     * Execute the Enter Expense Use Case.
+     * @param enterExpenseInputData the input data for this use case
+     */
+    void execute(EnterExpenseInputData enterExpenseInputData);
+
+}
---
+++ src/main/java/use_case/enter_expense/EnterExpenseInputData.java
@@ -0,0 +1,24 @@
+package use_case.enter_expense;
+
+/**
+ * The input data for the Enter Expense Use Case.
+ */
+public class EnterExpenseInputData {
+
+    private final String date;
+    private final double amount;
+
+    public EnterExpenseInputData(String date, double amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+
+    String getDate() {
+        return date;
+    }
+
+    double getAmount() {
+        return amount;
+    }
+
+}
