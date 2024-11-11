f0f478e8aa51ec5c0d7785382574e4ccecb18078
Annie Szorkin <antonsorkin3@gmail.com>
2024-11-10 23:45:59 -0500

    Implemented the CommonItem

--- src/main/java/entity/item/CommonItem.java
+++ src/main/java/entity/item/CommonItem.java
@@ -1,4 +1,32 @@
 package entity.item;
 
+/**
+ * A simple implementation of the Item interface
+ */
 public class CommonItem implements Item {
+
+    private final String name;
+    private final String date;
+    private final double amount;
+
+    public CommonItem(String name, String date, double amount) {
+        this.name = name;
+        this.date = date;
+        this.amount = amount;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getDate() {
+        return this.date;
+    }
+
+    @Override
+    public double getAmount() {
+        return this.amount;
+    }
 }
