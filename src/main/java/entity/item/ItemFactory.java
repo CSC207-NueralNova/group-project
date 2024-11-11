27b2d2053c7a0fd36ee1b0bd91092a87655c0d86
Annie Szorkin <antonsorkin3@gmail.com>
2024-11-10 23:41:54 -0500

    Added the ItemFacroty interface

--- src/main/java/entity/item/ItemFactory.java
+++ src/main/java/entity/item/ItemFactory.java
@@ -1,4 +1,15 @@
 package entity.item;
 
+/**
+ * Factory for creating new Items.
+ */
 public interface ItemFactory {
+    /**
+     * Creates a new Item instance with specified name, date and the amount.
+     * @param name The name of the Item.
+     * @param date The date of the transaction in format "MMYY".
+     * @param amount The amount of the transaction
+     * @return A new instance of Item with the specified parameters.
+     */
+    Item create(String name, String date, double amount);
 }
