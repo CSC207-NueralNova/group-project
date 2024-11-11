3ed7770f4c9feba8f416b42b419203b9daa78ca3
Annie Szorkin <antonsorkin3@gmail.com>
2024-11-10 23:47:16 -0500

    Implemented the CommonItemFactory

--- src/main/java/entity/item/CommonItemFactory.java
+++ src/main/java/entity/item/CommonItemFactory.java
@@ -1,4 +1,12 @@
 package entity.item;
 
+/**
+ * Factory for creating CommonItem objects.
+ */
 public class CommonItemFactory implements ItemFactory {
+
+    @Override
+    public Item create(String name, String date, double amount) {
+        return new CommonItem(name, date, amount);
+    }
 }
