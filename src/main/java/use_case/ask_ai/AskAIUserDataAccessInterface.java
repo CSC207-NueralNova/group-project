cba87bdcee1e23cb393a87f29f6534f940a5675a
James Han <hanlyu2005@gmail.com>
2024-11-12 16:21:41 -0500

    added Ask AI interactor, the Input Data class, and the Data Access Interface

--- src/main/java/use_case/ask_ai/AskAIUserDataAccessInterface.java
+++ src/main/java/use_case/ask_ai/AskAIUserDataAccessInterface.java
@@ -1,6 +1,15 @@
 package use_case.ask_ai;
 
-// TODO: make.
+import entity.user.User;
 
 public interface AskAIUserDataAccessInterface {
+
+    /**
+     * Finds a user by their username.
+     * @param username the username of the user to retrieve.
+     * @return the User object if found, null otherwise.
+     */
+    User findUserByUsername(String username);
+
+    // Additional methods can be added here if needed, e.g., updateUser, saveUser, etc.
 }
