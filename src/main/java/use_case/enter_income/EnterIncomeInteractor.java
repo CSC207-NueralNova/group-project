bbe449e09fe65c2238f6318452ffe3e16d839ba6
Anthony Zhao <superchess48@gmail.com>
2024-11-15 14:34:34 -0500

    implemented use case for enter income

--- src/main/java/use_case/enter_income/EnterIncomeInteractor.java
+++ src/main/java/use_case/enter_income/EnterIncomeInteractor.java
@@ -2,12 +2,15 @@ package use_case.enter_income;
 
 // TODO: make.
 
+import entity.item.CommonItemFactory;
 import entity.item.Item;
+import entity.item.ItemFactory;
 import use_case.enter_expense.EnterExpenseInteractor;
 
 public class EnterIncomeInteractor implements EnterIncomeInputBoundary{
     private final EnterIncomeUserDataAccessInterface userDataAccessObject;
     private final EnterIncomeOutputBoundary enterIncomePresenter;
+    private final ItemFactory itemFactory = new CommonItemFactory();
 
     public EnterIncomeInteractor(EnterIncomeUserDataAccessInterface userDataAccessObject,
                                  EnterIncomeOutputBoundary enterIncomePresenter) {
@@ -32,7 +35,7 @@ public class EnterIncomeInteractor implements EnterIncomeInputBoundary{
                     value + " is not a valid value for an income, please enter a positive value with up to two decimal points."
             );
         } else {
-            final Item item = ... // TODO: create Item w/ date type & value
+            final Item item = this.itemFactory.create(enterIncomeInputData.getType(), enterIncomeInputData.getValue());
             userDataAccessObject.save(item);
 
             final EnterIncomeOutputData enterIncomeOutputData = new EnterIncomeOutputData(
