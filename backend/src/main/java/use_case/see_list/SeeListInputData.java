a99c7054c3aa818e41acce2f525eb4af31c7d36e
Observer3510 <missjiayiwu@gmail.com>
2024-11-18 14:52:06 -0500

    Prepared initial code for the see list use case.

--- backend/src/main/java/use_case/see_list/SeeListInputData.java
+++ backend/src/main/java/use_case/see_list/SeeListInputData.java
@@ -3,4 +3,15 @@ package use_case.see_list;
 // TODO: make.
 
 public class SeeListInputData {
+
+    private final String date;
+
+    public SeeListInputData(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
 }
--- backend/src/main/java/use_case/see_list/SeeListUserDataAccessInterface.java
+++ backend/src/main/java/use_case/see_list/SeeListUserDataAccessInterface.java
@@ -2,5 +2,29 @@ package use_case.see_list;
 
 // TODO: make.
 
+import entity.monthly_spending.MonthlySpending;
+
 public interface SeeListUserDataAccessInterface {
+    /**
+     * Returns the username of the curren user of the application.
+     * @return the username of the current user; null indicates that no one is logged into the application.
+     */
+    String getCurrentUsername();
+
+    /**
+     * Checks if the MonthlySpending associated with the given user and date exists.
+     * @param username The username to returns the spending from.
+     * @param date The date of the monthly spending in the "MMYY" format.
+     * @return true if the spending exists, false otherwise.
+     */
+    boolean existsMonthlySpendingByUsernameAndDate(String username, String date);
+
+    /**
+     *
+     * Returns the MonthlySpending for a given user and a given date in format "MMYY"
+     * @param username The username to returns the spending from.
+     * @param date The date of the monthly spending in the "MMYY" format.
+     * @return The monthlySpending associated with the given data.
+     */
+    MonthlySpending getMonthlySpendingByUsernameAndDate(String username, String date);
 }
