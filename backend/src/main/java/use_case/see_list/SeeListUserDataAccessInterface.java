d9fd39cb95d6bb28f265457bdc4072f82c101297
Observer3510 <missjiayiwu@gmail.com>
2024-11-22 16:41:58 -0500

    added MonthlyIncome functions to the DAO

--- backend/src/main/java/use_case/see_list/SeeListUserDataAccessInterface.java
+++ backend/src/main/java/use_case/see_list/SeeListUserDataAccessInterface.java
@@ -2,6 +2,7 @@ package use_case.see_list;
 
 // TODO: add income functions.
 
+import entity.monthly_income.MonthlyIncome;
 import entity.monthly_spending.MonthlySpending;
 
 public interface SeeListUserDataAccessInterface {
@@ -27,4 +28,21 @@ public interface SeeListUserDataAccessInterface {
      * @return The monthlySpending associated with the given data.
      */
     MonthlySpending getMonthlySpendingByUsernameAndDate(String username, String date);
+
+    /**
+     * Checks if the MonthlyIncome associated with the given user and date exists.
+     * @param username The username to return the income from.
+     * @param date The date of the monthly income in the "MMYY" format.
+     * @return true if the income exists, false otherwise.
+     */
+    boolean existsMonthlyIncomeByUsernameAndDate(String username, String date);
+
+    /**
+     *
+     * Returns the MonthlyIncome for a given user and a given date in format "MMYY"
+     * @param username The username to returns the income from.
+     * @param date The date of the monthly income in the "MMYY" format.
+     * @return The monthlyIncome associated with the given data.
+     */
+    MonthlyIncome getMonthlyIncomeByUsernameAndDate(String username, String date);
 }
