476512d16bad8e174345e60effd266cfff976bc6
Observer3510 <missjiayiwu@gmail.com>
2024-11-25 13:12:35 -0500

    Renamed all instances of name to category

--- backend/src/main/java/use_case/enter_income/EnterIncomeInputData.java
+++ backend/src/main/java/use_case/enter_income/EnterIncomeInputData.java
@@ -4,18 +4,18 @@ package use_case.enter_income;
  * The Input Data for the EnterIncome Use Case
  */
 public class EnterIncomeInputData {
-    private final String name;
+    private final String category;
     private final String date;
     private final double value;
 
-    public EnterIncomeInputData(String name, String date, double value) {
-        this.name = name;
+    public EnterIncomeInputData(String category, String date, double value) {
+        this.category = category;
         this.date = date;
         this.value = value;
     }
 
-    public String getName() {
-        return name;
+    public String getCategory() {
+        return category;
     }
 
     public String getDate() {
