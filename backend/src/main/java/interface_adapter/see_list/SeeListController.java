ae3999cdfb6e05fc90858ba189e39c4d18a491a0
Observer3510 <missjiayiwu@gmail.com>
2024-11-25 12:10:27 -0500

    Added execute function to SeeListController

--- backend/src/main/java/interface_adapter/see_list/SeeListController.java
+++ backend/src/main/java/interface_adapter/see_list/SeeListController.java
@@ -1,6 +1,26 @@
 package interface_adapter.see_list;
 
-// TODO: make.
+import use_case.see_list.SeeListInputBoundary;
+import use_case.see_list.SeeListInputData;
 
+/**
+ * The controller for the See List Use Case.
+ */
 public class SeeListController {
+
+    private final SeeListInputBoundary seeListUseCaseInteractor;
+
+    public SeeListController(SeeListInputBoundary seeListUseCaseInteractor) {
+        this.seeListUseCaseInteractor = seeListUseCaseInteractor;
+    }
+
+    /**
+     * Executes the See List Use Case.
+     * @param date the month and year of the items that the user wants to see.
+     */
+    public void execute(String date) {
+        final SeeListInputData seeListInputData = new SeeListInputData(date);
+        this.seeListUseCaseInteractor.execute(seeListInputData);
+    }
+
 }
