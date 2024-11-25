c5dd18684f505a01b18b21645664dd02efc5c9fd
Annie Szorkin <antonsorkin3@gmail.com>
2024-11-25 18:00:10 -0500

    Fixed the autowiring with SeeListPresenter

--- backend/src/main/java/interface_adapter/see_list/SeeListPresenter.java
+++ backend/src/main/java/interface_adapter/see_list/SeeListPresenter.java
@@ -1,6 +1,19 @@
 package interface_adapter.see_list;
 
-// TODO: make.
 
-public class SeeListPresenter {
+import org.springframework.stereotype.Service;
+import use_case.see_list.SeeListOutputBoundary;
+import use_case.see_list.SeeListOutputData;
+
+@Service
+public class SeeListPresenter implements SeeListOutputBoundary {
+    @Override
+    public void prepareSuccessView(SeeListOutputData outputData) {
+        // temp code for testing
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage) {
+        // temp code for testing
+    }
 }
