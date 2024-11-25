dec7fa6e4817cd77ae5a647429325d95bbcaab0b
Anthony Zhao <superchess48@gmail.com>
2024-11-25 14:44:57 -0500

    Implemented enter income into User DAO

--- backend/src/main/java/data_access/InMemoryUserDataAccessObject.java
+++ backend/src/main/java/data_access/InMemoryUserDataAccessObject.java
@@ -3,10 +3,12 @@ package data_access;
 import java.util.HashMap;
 import java.util.Map;
 
+import entity.monthly_income.MonthlyIncome;
 import entity.monthly_spending.MonthlySpending;
 import entity.user.User;
 import use_case.change_password.ChangePasswordUserDataAccessInterface;
 import use_case.enter_expense.EnterExpenseUserDataAccessInterface;
+import use_case.enter_income.EnterIncomeUserDataAccessInterface;
 import use_case.login.LoginUserDataAccessInterface;
 import use_case.logout.LogoutUserDataAccessInterface;
 import use_case.signup.SignupUserDataAccessInterface;
@@ -19,10 +21,12 @@ public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterfa
         LoginUserDataAccessInterface,
         ChangePasswordUserDataAccessInterface,
         LogoutUserDataAccessInterface,
-        EnterExpenseUserDataAccessInterface {
+        EnterExpenseUserDataAccessInterface,
+        EnterIncomeUserDataAccessInterface {
 
     private final Map<String, User> users = new HashMap<>();
     private final Map<String, Map<String, MonthlySpending>> monthlySpendings = new HashMap<>();
+    private final Map<String, Map<String, MonthlyIncome>> monthlyIncome = new HashMap<>();
 
     private String currentUsername;
 
@@ -55,6 +59,33 @@ public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterfa
     @Override
     public String getCurrentUsername() {
         return this.currentUsername;
+    }
+
+    @Override
+    public boolean existsMonthlyIncomeByUsernameAndDate(String username, String date) {
+        if (monthlyIncome.containsKey(username)) {
+            return monthlyIncome.get(username).containsKey(date);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public MonthlyIncome getMonthlyIncomeByUsernameAndDate(String username, String date) {
+        if (monthlyIncome.containsKey(username)) {
+            return monthlyIncome.get(username).get(date);
+        }
+        return null;
+    }
+
+    @Override
+    public void writeMonthlyIncome(String username, MonthlyIncome monthlyIncome) {
+        // Add an empty hashmap if there is no username
+        if (!this.monthlyIncome.containsKey(username)) {
+            this.monthlyIncome.put(username, new HashMap<>());
+        }
+        // Overwrite the existing record for the monthlyIncome
+        this.monthlyIncome.get(username).put(monthlyIncome.getDate(), monthlyIncome);
     }
 
     @Override
