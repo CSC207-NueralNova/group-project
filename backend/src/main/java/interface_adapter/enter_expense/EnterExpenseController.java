3cafac40a0b57fcd98305da59e99f4d461adc66c
Annie Szorkin <antonsorkin3@gmail.com>
2024-11-25 14:06:34 -0500

    Added the controller for the EnterExpense use case mounted at enter_income/enter

--- backend/src/main/java/interface_adapter/enter_expense/EnterExpenseController.java
+++ backend/src/main/java/interface_adapter/enter_expense/EnterExpenseController.java
@@ -1,6 +1,27 @@
 package interface_adapter.enter_expense;
 
-// TODO: make.
+import org.springframework.web.bind.annotation.*;
+import use_case.enter_expense.EnterExpenseInteractor;
+import use_case.enter_expense.EnterExpenseInputData;
+import use_case.enter_expense.EnterExpenseOutputData;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@RestController
+@RequestMapping("/api/enter_income")
+@CrossOrigin(origins = "http://localhost:5173")  // Allow CORS requests from the frontend
 public class EnterExpenseController {
+
+    private final EnterExpenseInteractor enterExpenseInteractor;
+
+    @Autowired
+    public EnterExpenseController(EnterExpenseInteractor enterExpenseInteractor) {
+        this.enterExpenseInteractor = enterExpenseInteractor;
+    }
+
+    // Endpoint to handle chat messages
+    @PostMapping("/enter")
+    public EnterExpenseOutputData handleChatRequest(@RequestBody EnterExpenseInputData inputData) {
+        // Return the chatbot response as JSON
+        return enterExpenseInteractor.execute(inputData);
+    }
 }
