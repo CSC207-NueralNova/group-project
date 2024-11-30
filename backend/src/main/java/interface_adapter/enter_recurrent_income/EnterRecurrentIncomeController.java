70a1fb7e79137320812a2556cf437b077a1fedb1
Annie Szorkin <antonsorkin3@gmail.com>
2024-11-30 16:18:58 -0500

    Removed unused imports, fixed comments

--- backend/src/main/java/interface_adapter/enter_recurrent_income/EnterRecurrentIncomeController.java
+++ backend/src/main/java/interface_adapter/enter_recurrent_income/EnterRecurrentIncomeController.java
@@ -2,9 +2,6 @@ package interface_adapter.enter_recurrent_income;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import use_case.enter_income.EnterIncomeInputBoundary;
-import use_case.enter_income.EnterIncomeInputData;
-import use_case.enter_income.EnterIncomeOutputData;
 import use_case.enter_recurrent_income.EnterRecurrentIncomeInputBoundary;
 import use_case.enter_recurrent_income.EnterRecurrentIncomeInputData;
 import use_case.enter_recurrent_income.EnterRecurrentIncomeOutputData;
@@ -21,10 +18,9 @@ public class EnterRecurrentIncomeController {
         this.enterIncomeInteractor = enterRecurrentIncomeInteractor;
     }
 
-    // Endpoint to handle enter income request
+    // Endpoint to handle enter recurrent income request
     @PostMapping("/enter")
     public EnterRecurrentIncomeOutputData handleEnterIncomeRequest(@RequestBody EnterRecurrentIncomeInputData inputData) {
-        // Return the chatbot response as JSON
         return enterIncomeInteractor.execute(inputData);
     }
 }
