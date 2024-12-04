ff91c447de39dff3c04c1631af1544d0c170689e
LIM YSAJELLE YI RU <ysajelleyiru.lim@mail.utoronto.ca>
2024-12-03 23:43:12 -0500

    Fixed style issues for item_income entity

--- backend/src/main/java/entity/item_income/ItemIncomeFactory.java
+++ backend/src/main/java/entity/item_income/ItemIncomeFactory.java
@@ -7,6 +7,7 @@ public interface ItemIncomeFactory {
     /**
      * Creates a new ItemIncome instance with specified value.
      * @param value The value of the transaction
+     * @param date The date of the transaction
      * @return A new instance of Item with the specified parameters.
      */
     ItemIncome create(double value, String date);
